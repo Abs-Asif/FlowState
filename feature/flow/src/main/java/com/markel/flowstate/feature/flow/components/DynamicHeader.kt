465172c8411ab1555bbbf2ed4f62a93fa038e516
Markel15 <Enemar.h@gmail.com>
2026-05-18 15:32:06 +0200

    fix: padding in devices with some type of notches #15
    I changed the hardcoded value of 30 dp of padding to use the status Bar padding, this should change between different devices, but even if it makes every app padding different should be better because some devices need special paddings.

--- feature/flow/src/main/java/com/markel/flowstate/feature/flow/components/DynamicHeader.kt
+++ feature/flow/src/main/java/com/markel/flowstate/feature/flow/components/DynamicHeader.kt
@@ -1,8 +1,6 @@
 package com.markel.flowstate.feature.flow.components
 
 import android.content.res.Configuration
-import androidx.compose.animation.AnimatedContent
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
@@ -11,26 +9,21 @@ import androidx.compose.animation.expandVertically
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkVertically
-import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -38,7 +31,6 @@ import com.markel.flowstate.feature.tasks.R
 import java.time.LocalDate
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
-import java.util.Locale
 import androidx.compose.ui.platform.LocalLocale
 import androidx.compose.ui.text.font.FontStyle
 
@@ -60,14 +52,12 @@ fun DynamicHeader(isMinimized: Boolean)
         animationSpec = spring(stiffness = Spring.StiffnessLow),
         label = "height"
     )
-    val configuration = LocalConfiguration.current
-    val isLandscape = configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
 
     Column(
         modifier = Modifier
             .fillMaxWidth()
+            .statusBarsPadding()
             .padding(horizontal = 24.dp)
-            .padding(top = if (isLandscape) 6.dp else 30.dp)
     ) {
         Box(
             modifier = Modifier
