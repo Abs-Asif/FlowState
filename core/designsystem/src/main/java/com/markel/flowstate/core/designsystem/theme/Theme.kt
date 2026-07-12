5f066332de11b1608dbe64393e96590fe478039b
Markel15 <Enemar.h@gmail.com>
2026-07-12 21:31:49 +0200

    feat: new typography
    More expressive typography.

--- core/designsystem/src/main/java/com/markel/flowstate/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/markel/flowstate/core/designsystem/theme/Theme.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.MaterialExpressiveTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MotionScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
@@ -335,7 +336,8 @@ fun FlowStateTheme(
     ) {
         MaterialExpressiveTheme(
             colorScheme = colorScheme,
-            typography = Typography,
+            typography = FlowStateTypography(),
+            motionScheme = MotionScheme.expressive(),
             content = content
         )
     }
